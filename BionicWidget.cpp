dcc86e850d425da7cf7d3d2f63c122171457d88f
Siegfried Nowhere <sigifredo89@gmail.com>
2024-07-05 00:51:15 -0500

    Se centra la ventana al iniciar.

--- BionicWidget.cpp
+++ BionicWidget.cpp
@@ -12,11 +12,12 @@ void BionicWidget::setBionicText(const QString& sText)
     QString sResult;
     QStringList words = sText.split(" ");
 
-    for (const QString &sWord : words) {
+    for (const QString &sWord : words)
+    {
         int iLen = sWord.length();
         int iBoldPart = iLen / 2;
 
-        sResult.append("<b>" + sWord.left(iBoldPart) + "</b>" + sWord.mid(iBoldPart) + " ");
+        sResult.append("<strong>" + sWord.left(iBoldPart) + "</strong>" + sWord.mid(iBoldPart) + " ");
     }
 
     sResult.replace("\r\n", "<br>");
--- main.cpp
+++ main.cpp
@@ -1,23 +1,51 @@
+// Own
 #include "MainWindow.hpp"
 
+// Qt
 #include <QApplication>
 #include <QLocale>
+#include <QScreen>
 #include <QTranslator>
+
+QRect centerGeometry();
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
 
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
+
+    for (const QString &locale : uiLanguages)
+    {
         const QString baseName = "teleprompter_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
-            a.installTranslator(&translator);
+
+        if (translator.load(":/i18n/" + baseName))
+        {
+            app.installTranslator(&translator);
             break;
         }
     }
-    MainWindow w;
-    w.show();
-    return a.exec();
+
+    MainWindow mw;
+    mw.setGeometry(centerGeometry());
+    mw.show();
+
+    return app.exec();
+}
+
+QRect centerGeometry()
+{
+    QRect screenGeometry = QGuiApplication::primaryScreen()->geometry();
+
+    int screenWidth = screenGeometry.width();
+    int screenHeight = screenGeometry.height();
+
+    int windowWidth = screenWidth * 0.3;
+    int windowHeight = screenHeight / 2;
+
+    int posX = (screenWidth - windowWidth) / 2;
+    int posY = (screenHeight - windowHeight) / 2;
+
+    return QRect(posX, posY, windowWidth, windowHeight);
 }
